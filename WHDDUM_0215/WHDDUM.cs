95a87e04fd5b85d829c1b3d3f465c1b5baeca66f
sdavid172 <90756627+sdavid172@users.noreply.github.com>
2022-02-25 12:04:51 +0100

    Add files via upload

---
+++ WHDDUM_0215/WHDDUM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace WHDDUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("vezeteknev.txt");
+
+            sw.WriteLine("Siska Dávid");
+            sw.WriteLine("Gazdaságinformatikus");
+            sw.WriteLine("WHDDUM");
+           
+
+            sw.Close();
+            
+           
+            StreamReader sr = new StreamReader("vezeteknev.txt");
+
+
+            string text = sr.ReadLine();
+            while (text != null)
+            {
+                Console.WriteLine(text);
+
+                text = sr.ReadLine();
+
+            }
+            sr.Close();
+
+
+           
+
+        }
+    }
+}
